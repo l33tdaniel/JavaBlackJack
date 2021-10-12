ee991b8b02251d0904c47aa59b38f1652695b117
Daniel <83420777+l33tdaniel@users.noreply.github.com>
2021-10-12 17:14:22 -0500

    Update Card.java
    tried to fix some spacing

--- Card.java
+++ Card.java
@@ -1,58 +1,51 @@
-public class Card{
+public class Card {
 	private String suit;
 	private int number;
-
 //Initialize Card with a suit and a number
-  public Card(String suit, int number){
-			this.suit = suit;
-			this.number = number;
+	public Card(String suit, int number){
+		this.suit = suit;
+		this.number = number;
 	}
-
 //Getter for number variable
-  public int getNumber() {
+ 	public int getNumber() {
 		if(number <= 11){
-    return this.number; 
+    			return this.number; 
 		}else{
 			return 10;
-		}
-  }
-
+	 	}
+  	}
 //Set number
-  public void setNumber(int number) {
-    this.number = number; 
-  }
+	public void setNumber(int number) {
+    		this.number = number; 
+  	}
 
 //Print Method  
 	public String asString(int i){
-    String suitSymbol = "";
-    if(suit == "spades"){
+    		String suitSymbol = "";
+    		if(suit == "spades"){
 			suitSymbol = "♠ ";
-		} else if(suit == "hearts"){
-			suitSymbol = "♥ ";
-		} else if(suit == "diamonds"){
-			suitSymbol = "♦ ";
-		}else{
-			suitSymbol = "♣ ";
+			} else if(suit == "hearts"){
+				suitSymbol = "♥ ";
+			} else if(suit == "diamonds"){
+				suitSymbol = "♦ ";
+			}else{
+				suitSymbol = "♣ ";
+			}
+   		String numberString = "";
+  		if(number < 10){
+			numberString = "" + number + " "; 
+    			}else if(number == 10){
+      				numberString = "" + number;
+			}else if(number == 11){
+				numberString = "A ";
+			}else if(number == 12){
+				numberString = "J ";
+			}else if(number == 13){
+				numberString = "K ";
+			}else if(number == 14){
+				numberString = "Q ";
 		}
-    String numberString = "";
-
-  	if(number < 10){
-			numberString = "" + number + " ";
-      
-    }else if(number == 10){
-      numberString = "" + number;
-    
-		}else if(number == 11){
-			numberString = "A ";
-		}else if(number == 12){
-			numberString = "J ";
-		}else if(number == 13){
-			numberString = "K ";
-		}else if(number == 14){
-			numberString = "Q ";
-		}
-
-    String[] toReturn = new String[]{
+   	 String[] toReturn = new String[]{
       "┌──────┐",
       "│ " + numberString + " " + suitSymbol + "│",
       "│      │",
